54b633451dcfaa7c53bfb8a5b511bbb790204589
Anthony Rurup <tcrurup@gmail.com>
2019-09-27 18:11:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,4 +18,8 @@ function BoardMember(name, homeState, training){
   this.releasePressStatement = function(){
     return "You will see great things from Scuber."
   }
+  
+  this.sayHi = function(){
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}`
+  }
 }

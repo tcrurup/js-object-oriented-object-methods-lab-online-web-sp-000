c8357e7b7dd1ecbd52d68a81ec0c9daf6dddc20c
Anthony Rurup <tcrurup@gmail.com>
2019-09-27 18:10:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,17 @@ function BoardMember(name, homeState, training){
   
   this.veto = function(){
     return "No, I must disagree";
+  };
+  
+  this.approve = function(){
+    return "You can do that!";
+  };
+  
+  this.doCharity = function(){
+    return "I like to help people.";
+  };
+  
+  this.releasePressStatement(){
+    return "You will see great things from Scuber"
   }
 }
